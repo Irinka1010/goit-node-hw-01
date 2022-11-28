498401603f800844ce8c3c35d1a7b1928d03ff2e
dimon4uk1990 <mexal07@ukr.net>
2022-11-28 20:15:37 +0200

    fix  json

--- contacts/getContactById.js
+++ contacts/getContactById.js
@@ -3,9 +3,7 @@ const list = require('./listContacts');
 const getContactById = async id => {
   const contacts = await list();
   const result = contacts.find(item => item.id === id);
-  if (!result) {
-    return null;
-  }
-  return result;
+
+  return result || null;
 };
 module.exports = getContactById;
